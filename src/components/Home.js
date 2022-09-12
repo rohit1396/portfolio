952d5bcd660de8e8d7caf6815139d72705edfad0
Rohit _Gaikwad <rohitgaikwad726@gmail.com>
2022-09-12 21:13:09 +0530

    Home Changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,34 @@
 import React from "react";
 import "./Home.css";
+import Project from "./Project";
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="home_content">
-        <h1>Hi, I am Rohit Gaikwad - A Computer Engineer</h1>
-        <p>
-          I have a Bachelors Degree in Computer Engineering From Mumbai
-          University.
-        </p>
-        <p>I enjoy making Web Pages and learning new skills.</p>
-        <p>
-          I believe a good developer is always developing new skills, accept
-          challenges and seek feedback in order to grow
-        </p>
-        <p>
-          My aim is to get placed in a good IT firm with some decent package.
-        </p>
+    <>
+      <div className="home">
+        <div className="home_content">
+          <h1>Hi, I am Rohit Gaikwad - A Computer Engineer</h1>
+          <p>
+            I have a Bachelors Degree in Computer Engineering From Mumbai
+            University.
+          </p>
+          <p>I enjoy making Web Pages and learning new skills.</p>
+          <p>I mostly work with ReactJs.</p>
+          <p>
+            I believe a good developer is always developing new skills, accept
+            challenges and seek feedback in order to grow
+          </p>
+          <p>
+            My aim is to get placed in a good IT firm with some decent package.
+          </p>
+        </div>
+        <img
+          className="home_logo"
+          src="..\images\developer_med.gif"
+          alt="home_logo"
+        />
       </div>
-      <img
-        className="home_logo"
-        src="..\images\developer_med.gif"
-        alt="home_logo"
-      />
-    </div>
+    </>
   );
 };
 
