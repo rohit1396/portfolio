2215aef4589c31e7229042998e61c4258aae3bd1
Rohit _Gaikwad <rohitgaikwad726@gmail.com>
2022-09-13 21:04:32 +0530

    Project Comp Change

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,35 @@
 import React from "react";
+import "./Project.css";
 
 const Project = () => {
-  return <div>Project</div>;
+  return (
+    <div className="project">
+      <div className="project_info">
+        <a href="https://to-do-list-rg.netlify.app/" target="_blank">
+          <img
+            className="project_img"
+            src="..\images\proj1.png"
+            alt="to-do_list"
+          />
+        </a>
+        <h3 className="project_title">To-do List App</h3>
+        <p>A simple To-Do List App </p>
+        <p>Functionalities : ADD, UPDATE, DELETE</p>
+      </div>
+      <div className="project_info">
+        <a href="https://netflix-rg.netlify.app/" target="_blank">
+          <img
+            className="project_img"
+            src="..\images\proj2.png"
+            alt="Netflix_Clone"
+          />
+        </a>
+        <h3 className="project_title">Netflix Clone</h3>
+        <p>A Netflix Clone displaying Various categories of Movies.</p>
+        <p>User Authentication With Firebase.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
